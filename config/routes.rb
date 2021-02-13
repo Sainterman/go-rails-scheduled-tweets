17b7a2eed7c5d71486da29086148b3a81dc44d4e
Santiago Castillo <sancastillog@gmail.com>
2021-02-12 22:00:26 -0500

    created 2 rooutes, edit and new, added navbar and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
+# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "new", to: "new#index", as: :new
+
+  get "edit", to: "edit#index", as: :edit
+  
+  root to: "main#index"
 end
